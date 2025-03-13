2958b0f8b38c69fe7f4d8a1782f5d8261945ffe9
carrils <scarrillo578@gmail.com>
2025-03-13 13:10:00 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import csv
+import json
+from datetime import datetime
+import pandas as pd
+import math
+import glob
+import os
+
+from mii import StaffAllocation
+from mii import ActivePeople
+from mii import DepartmentTags
+from mii import HROrganization
+from mii import TechStature
+
+from box import box
+
+
+def main():
+    print("yooo")
+
+
+if __name__ == "__main__":
+    main()
