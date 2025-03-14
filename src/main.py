ad057c7df0d2ade41f7b41ff7a5259f060a19603
carrils <scarrillo578@gmail.com>
2025-03-13 20:24:02 -0500

    lol this was the way to do it from the beginning

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import pandas as pd
 import math
 import glob
 import os
+import lxml  # lol pd.read_tml
 
 
 def integerizer(soup):
@@ -15,24 +16,39 @@ def integerizer(soup):
 
 
 def main():
-    # savings_df.loc[4] == total_df.loc[4]
-    savings_df = pd.read_table("../files/contracts_savings.txt")
-    total_df = pd.read_table("../files/contracts_total_value.txt")
+    # # savings_df.loc[4] == total_df.loc[4]
+    # savings_df = pd.read_table("../files/contracts_savings.txt")
+    # total_df = pd.read_table("../files/contracts_total_value.txt")
+    #
+    # # integerize THIS!
+    # total_df["Value"] = total_df["Value"].apply(integerizer)
+    # savings_df["Saved"] = savings_df["Saved"].apply(integerizer)
+    #
+    # # Total Value: The potential expenditure (with options).
+    # # Savings: Total value - current obligation
+    # obbies = total_df["Value"] - savings_df["Saved"]
+    #
+    # # so hows about we actually read this data
+    # super_df = pd.concat([total_df, savings_df["Saved"]], axis=1)
+    # super_df = pd.concat([super_df, obbies], axis=1)
+    # super_df.rename(columns={0: 'Obligations'}, inplace=True)
+    # super_df.to_csv("../files/super_doge.csv", index=False)
+    # print("Savings: Total value - current obligation")
+    # print("Total Value: The potential expenditure (with options).")
+    savings_html_thing = pd.read_html("files/Savings/Savings.html")
+    total_value_html_thing = pd.read_html("files/Total_Value/Total_Value.html")
 
-    # integerize THIS!
-    total_df["Value"] = total_df["Value"].apply(integerizer)
-    savings_df["Saved"] = savings_df["Saved"].apply(integerizer)
+    # Contracts: 4083 contract terminations totaling ~$15B in savings.
+    # contracts_total_value
+    # contracts_savings
+    # Grants: 6289 grant terminations totaling ~$15B in savings. Descriptions are forthcoming.
+    # grants_total_value
+    # grants_savings
+    # Real Estate: 747 lease terminations totaling 9,517,975 square feet and ~$468M in lease savings.
+    # real_estate_total_value
+    # real_estate_savings
 
-    # Total Value: The potential expenditure (with options).
-    # Savings: Total value - current obligation
-    obbies = total_df["Value"] - savings_df["Saved"]
-    
-    # so hows about we actually read this data
-    super_df = pd.concat([total_df, savings_df["Saved"]], axis=1)
-    super_df = pd.concat([super_df, obbies], axis=1)
-    super_df.to_csv("../files/super_doge.csv", index=False)
-    print("Savings: Total value - current obligation")
-    print("Total Value: The potential expenditure (with options).")
+    # go a little harder on the html files to extract the link here
 
 
 if __name__ == "__main__":
