57fb92ec3d8236a137da2d7e38ee28467178b9e1
carrils <scarrillo578@gmail.com>
2025-03-17 21:05:12 -0500

    subtract cruft, add archive

--- src/main.py
+++ src/main.py
@@ -17,30 +17,8 @@ def integerizer(soup):
 
 
 def main():
-    # # savings_df.loc[4] == total_df.loc[4]
-    # savings_df = pd.read_table("../files/contracts_savings.txt")
-    # total_df = pd.read_table("../files/contracts_total_value.txt")
-    #
-    # # integerize THIS!
-    # total_df["Value"] = total_df["Value"].apply(integerizer)
-    # savings_df["Saved"] = savings_df["Saved"].apply(integerizer)
-    #
-    # # Total Value: The potential expenditure (with options).
-    # # Savings: Total value - current obligation
-    # obbies = total_df["Value"] - savings_df["Saved"]
-    #
-    # # so hows about we actually read this data
-    # super_df = pd.concat([total_df, savings_df["Saved"]], axis=1)
-    # super_df = pd.concat([super_df, obbies], axis=1)
-    # super_df.rename(columns={0: 'Obligations'}, inplace=True)
-    # super_df.to_csv("../files/super_doge.csv", index=False)
-    # print("Savings: Total value - current obligation")
-    # print("Total Value: The potential expenditure (with options).")
-    # To get these html files go to doge.gov and expand all 3 tables, then save as.
-    # flip the little thing at the top to the other thing and then repeat
-    # thing == list of dataframes.
-    savings_html_thing = pd.read_html("files/Savings/Savings.html")  # 3.13.25
-    total_value_html_thing = pd.read_html("files/Total_Value/Total_Value.html")  # 3.13.25
+    savings_html_thing = pd.read_html("files/Savings/Savings.html")
+    total_value_html_thing = pd.read_html("files/Total_Value/Total_Value.html")
 
     # Contracts: 4083 contract terminations totaling ~$15B in savings.
     contracts_total_value = total_value_html_thing[0]
@@ -53,11 +31,11 @@ def main():
     real_estate_savings = savings_html_thing[2]
 
     # go a little harder on the html files to extract the link here
-    with open('files/Savings/Savings.html', 'r', encoding='utf-8') as file:
+    with open('files/Savings/Savings_3-13-25.html', 'r', encoding='utf-8') as file:
         html_content = file.read()
     savings_soup = BeautifulSoup(html_content, 'html.parser')
 
-    with open('files/Total_Value/Total_Value.html', 'r', encoding='utf-8') as file:
+    with open('files/Total_Value/Total_Value_3-13-25.html', 'r', encoding='utf-8') as file:
         html_content = file.read()
     value_soup = BeautifulSoup(html_content, 'html.parser')
 
