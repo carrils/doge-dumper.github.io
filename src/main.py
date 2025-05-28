32ef265e554fb13a2961e23fbacc47796ab4e376
carrils <scarrillo578@gmail.com>
2025-05-27 22:05:08 -0500

    is this all u r 2 me

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ import glob
 import os
 import requests
 
+
 # Plan:
 # read first page then determine how many times to do the curl
 # then consume that json into a df and then you have the contracts.
@@ -22,19 +23,49 @@ def integerizer(soup):
 
 
 def main():
-    contracts_500 = requests.get("https://api.doge.gov/savings/contracts?page=1&per_page=500").json()  # can also do .text and .content
-    contracts_500_json_meta = contracts_500['meta']  # {'total_results': 10248, 'pages': 22}
+    contracts_500 = requests.get(
+        "https://api.doge.gov/savings/contracts?page=1&per_page=500").json()  # can also do .text and .content
+    contracts_500['meta']  # {'total_results': 10248, 'pages': 22}
     contracts_500['meta']['pages']  # 22
 
-    page = 0
-    bin = []  # bin of jsons
-    while page <= contracts_500['meta']['pages']:
-        bin_json = requests.get(f"https://api.doge.gov/savings/contracts?&page={page}&per_page=500").json()
-        if bin_json['success']:  # for some reason only the first try returns a schema validation error who cares fuck elon musk
-            bin.append(bin_json['result']['contracts'])
-        page += 1
+    # grants: https://api.doge.gov/savings/grants?page=1&per_page=500
+    # leases: https://api.doge.gov/savings/leases?page=1&per_page=500
 
-    bin_df = pd.DataFrame.from_records(bin)
+    grants_500 = requests.get('https://api.doge.gov/savings/grants?page=1&per_page=500').json()
+
+    cpage = 1
+    cbin = []  # bin of jsons. jsons, once existing, go into the bin.
+    while cpage <= contracts_500['meta']['pages']:
+        bin_json = requests.get(f"https://api.doge.gov/savings/contracts?&page={cpage}&per_page=500").json()
+        cbin.append(pd.DataFrame.from_records(bin_json['result']['contracts']))
+        cpage += 1
+
+    contract_bin_df = pd.concat(cbin)
+
+    gpage = 1
+    gbin = []  # bin of jsons. jsons, once existing, go into the bin.
+    while gpage <= grants_500['meta']['pages']:
+        bin_json = requests.get(f"https://api.doge.gov/savings/grants?page={gpage}&per_page=500").json()
+        gbin.append(pd.DataFrame.from_records(bin_json['result']['grants']))
+        gpage += 1
+
+    grant_bin_df = pd.concat(gbin)
+
+    leases_500 = requests.get('https://api.doge.gov/savings/leases?page=1&per_page=500').json()
+    leases_500_df = pd.DataFrame.from_records(leases_500['result']['leases'])
+    lpage = 1
+    lbin = []  # bin of jsons. jsons, once existing, go into the bin.
+    while lpage <= leases_500['meta']['pages']:
+        bin_json = requests.get(f"https://api.doge.gov/savings/leases?&page={lpage}&per_page=500").json()
+        lbin.append(pd.DataFrame.from_records(bin_json['result']['leases']))
+        lpage += 1
+
+    lease_bin_df = pd.concat(lbin)
+
+    print(len(contract_bin_df))
+    print(len(grant_bin_df))
+    print(len(lease_bin_df))
+
 
 if __name__ == "__main__":
     main()
